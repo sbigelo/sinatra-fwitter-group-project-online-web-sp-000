78aa56de37fadf93700b808a87149d3db0e700e1
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 19:40:58 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,11 +13,15 @@ class UsersController < ApplicationController
    end
  end
 
-  # post '/signup' do
-  # if logged_in?
-  #     erb :'/index'
-  # else
-  #   end
-  # end
+ post '/signup' do
+     if params[:username] == "" || params[:email] == "" || params[:password] == ""
+       redirect to '/signup'
+     else
+       @user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+       @user.save
+       session[:user_id] = @user.id
+       redirect to '/tweets'
+     end
+   end
 
 end
