fc64f9e72262afd7e678997bf547116edf410795
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 19:54:00 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   get '/signup' do
     if !logged_in?
-     erb :'tweets/create_user', locals: {message: "Please sign up before you sign in"}
+     erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
    else
      redirect to '/tweets'
    end
