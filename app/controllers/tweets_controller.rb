c9ff564bc51adc2d93df0debebca2aab1243488f
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 18:24:29 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,24 @@
 class TweetsController < ApplicationController
 
+  get '/tweets' do
+    if logged_in?
+      @tweets = Tweet.all
+      erb :'/tweets/tweets'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/new' do
+    if logged_in?
+    erb :'/tweets/create_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  # post '/tweets' do
+  #
+  # end
 
 end
