1006efcd48d390e72769b6a7803bb9e8192142aa
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 20:36:15 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,7 +2,7 @@ class TweetsController < ApplicationController
   get '/tweets' do
     if logged_in?
       @tweets = Tweet.all
-      erb :'/tweets'
+      erb :'tweets/tweets'
     else
       redirect to '/login'
     end
