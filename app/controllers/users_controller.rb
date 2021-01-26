4b40691b6c615f58d43e429363e95a0581290a65
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 19:49:44 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,5 +23,31 @@ class UsersController < ApplicationController
        redirect to '/tweets'
      end
    end
+   
+   get '/login' do
+       if !logged_in?
+         erb :'users/login'
+       else
+         redirect to '/tweets'
+       end
+     end
 
-end
+     post '/login' do
+       user = User.find_by(:username => params[:username])
+       if user && user.authenticate(params[:password])
+         session[:user_id] = user.id
+         redirect to "/tweets"
+       else
+         redirect to '/signup'
+       end
+     end
+
+     get '/logout' do
+       if logged_in?
+         session.destroy
+         redirect to '/login'
+       else
+         redirect to '/'
+       end
+     end
+   end
