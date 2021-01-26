2d002d9e8ea6d2f2c507cb79d06836b8977a6829
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 19:12:23 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -19,19 +19,32 @@ class TweetsController < ApplicationController
 
   post '/tweets' do
     if logged_in?
-    if params[:content] == ""
-      redirect to "/tweets/new"
-    else
-      @tweet = current_user.tweets.build(content: params[:content])
-      if @tweet.save
-        redirect to "/tweets/#{@tweet.id}"
-      else
+      if params[:content] == ""
         redirect to "/tweets/new"
+      else
+        @tweet = current_user.tweets.build(content: params[:content])
+        if @tweet.save
+          redirect to "/tweets/#{@tweet.id}"
+        else
+          redirect to "/tweets/new"
+        end
       end
+    else
+      redirect to '/login'
     end
-  else
-    redirect to '/login'
   end
-end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      erb :'/login'
+    end
+  end
+
+  # post '/tweets/:id/edit' do
+  #
+  # end
 
 end
