a00a293a7f9da8ff9cacdf201f371708acf8b705
Sean Bigelow <sean.bigelow1@gmail.com>
2021-01-26 18:47:29 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,23 @@
 class UsersController < ApplicationController
 
-  get '/signup' do
-    erb :'/users/create_user'
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :'users/show'
   end
 
-  post '/signup' do
-    redirect to '/index'
-  end
+  get '/signup' do
+    if !logged_in?
+     erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+   else
+     redirect to '/tweets'
+   end
+ end
+
+  # post '/signup' do
+  # if logged_in?
+  #     erb :'/index'
+  # else
+  #   end
+  # end
 
 end
